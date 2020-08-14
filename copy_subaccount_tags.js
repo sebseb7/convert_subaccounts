36a957d3dac82630f9130f0a958dddae15f36df1
tubedev2000 <tubedev2000@gmail.com>
2020-08-14 20:09:24 +0200

    fix

--- copy_subaccount_tags.js
+++ copy_subaccount_tags.js
@@ -10,7 +10,7 @@ api.rpc('127.0.0.1',tube3port,'get_accounts',{},function(err,result){
 
 	async.mapSeries(result.subaddress_accounts, function(account, callback) {
 		console.log('index '+account.account_index+' tag: '+account.tag);
-		api.rpc('127.0.0.1',tube4port,'tag_accounts',{"tag":account.tag,"accounts":[result.account_index]},function(err,result){
+		api.rpc('127.0.0.1',tube4port,'tag_accounts',{"tag":account.tag,"accounts":[account.account_index]},function(err,result){
 			callback(null,account);
 		});
 	});
